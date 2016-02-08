209648b623d12a79aba2f22dae82245d6c2106b8
Aleksey Ilyin <alekseyilinprog@gmail.com>
2016-02-08 17:29:09 +0300

    Add Gyms tab to admin page

--- usepoll/admin.py
+++ usepoll/admin.py
@@ -3,15 +3,13 @@ from django.contrib import admin
 from django.contrib import admin
 
 from .models import Event
-from .models import Exercise
-from .models import Customer
-from .models import Coach
-
-admin.site.register(Event)
+from .models import Exercise, Customer, Coach, Gym
 
 admin.site.register(Exercise)
 
 admin.site.register(Customer)
 
 admin.site.register(Coach)
+
+admin.site.register(Gym)
 # Register your models here.
